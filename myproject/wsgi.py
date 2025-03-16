66794f99feed95e50ca3eb5f1def8891050be56b
thisis0123 <jacknoam0@gmail.com>
2025-03-16 08:18:33 +0200

    some new settings

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -7,10 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
