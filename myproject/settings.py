fc60f5bfb16cedaeb6fca5012c1cc8da037b9dde
thisis0123 <jacknoam0@gmail.com>
2025-03-16 10:06:37 +0200

    added whitenoise

--- myproject/settings.py
+++ myproject/settings.py
@@ -130,12 +130,13 @@ STATICFILES_DIRS= [
 ]
 
 
-STORAGES = {
+STATICFILES_STORAGES = {
     # ...
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
